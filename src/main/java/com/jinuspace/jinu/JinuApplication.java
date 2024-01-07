f43ed8c2cd7698c2668db91a6b4f0c6749badab8
jinu <wlsdn147@naver.com>
2024-01-07 22:21:51 +0900

    feat : 초기 설정

---
+++ src/main/java/com/jinuspace/jinu/JinuApplication.java
@@ -0,0 +1,13 @@
+package com.jinuspace.jinu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinuspace/jinu/JinuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinuspace.jinu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
